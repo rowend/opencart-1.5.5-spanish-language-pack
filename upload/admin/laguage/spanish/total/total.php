8ecdc4485f23de792ce6705fcf4168f41c9593d4
rowend <rowend07@gmail.com>
2013-01-27 22:02:34 -0600

    total file

--- upload/admin/laguage/spanish/total/total.php
+++ upload/admin/laguage/spanish/total/total.php
@@ -4,7 +4,7 @@ $_['heading_title']    = 'Total';
 
 // Text
 $_['text_total']       = 'Totales del pedid';
-$_['text_success']     = 'Éxito: has modificado el total!';
+$_['text_success']     = 'Éxito: has modificado los totales!';
 
 // Entry
 $_['entry_status']     = 'Estado:';
